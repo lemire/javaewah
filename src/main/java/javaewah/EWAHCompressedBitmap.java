630a498fd4c826c4a3722b53754497f9cab164de
Jason Davies <jason@jasondavies.com>
2012-01-04 22:06:29 +0000

    Fix minor typos in comments.

--- src/main/java/javaewah/EWAHCompressedBitmap.java
+++ src/main/java/javaewah/EWAHCompressedBitmap.java
@@ -75,7 +75,7 @@ public final class EWAHCompressedBitmap implements Cloneable, Externalizable,
   }
 
   /**
-   * Returns a new compressed bitmap contained the bitwise XOR values of the
+   * Returns a new compressed bitmap containing the bitwise XOR values of the
    * current bitmap with some other bitmap.
    * 
    * The running time is proportional to the sum of the compressed sizes (as
@@ -189,7 +189,7 @@ public final class EWAHCompressedBitmap implements Cloneable, Externalizable,
   }
 
   /**
-   * Returns a new compressed bitmap contained the bitwise AND values of the
+   * Returns a new compressed bitmap containing the bitwise AND values of the
    * current bitmap with some other bitmap.
    * 
    * The running time is proportional to the sum of the compressed sizes (as
@@ -292,8 +292,8 @@ public final class EWAHCompressedBitmap implements Cloneable, Externalizable,
   }
 
   /**
-   * Returns a new compressed bitmap contained the bitwise AND NOT values of the
-   * current bitmap with some other bitmap.
+   * Returns a new compressed bitmap containing the bitwise AND NOT values of
+   * the current bitmap with some other bitmap.
    * 
    * The running time is proportional to the sum of the compressed sizes (as
    * reported by sizeInBytes()).
@@ -433,7 +433,7 @@ public final class EWAHCompressedBitmap implements Cloneable, Externalizable,
   }
 
   /**
-   * Returns a new compressed bitmap contained the bitwise OR values of the
+   * Returns a new compressed bitmap containing the bitwise OR values of the
    * current bitmap with some other bitmap.
    * 
    * The running time is proportional to the sum of the compressed sizes (as
