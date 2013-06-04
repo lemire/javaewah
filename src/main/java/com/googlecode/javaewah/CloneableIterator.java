ef9fd803e382659b07e867fe89113ba6a1a3fe4a
Daniel Lemire <lemire@gmail.com>
2013-06-04 11:34:44 -0400

    Some missing files

---
+++ src/main/java/com/googlecode/javaewah/CloneableIterator.java
@@ -0,0 +1,24 @@
+package com.googlecode.javaewah;
+
+/**
+ * Like a standard Java iterator, except that you can clone it.
+ *
+ * @param <E> the data type of the iterator
+ */
+public interface CloneableIterator<E> extends Cloneable {
+	
+	/**
+	 * @return whether there is more
+	 */
+	public boolean hasNext();
+	/**
+	 * @return the next element
+	 */
+	public E next();
+	/**
+	 * @return a copy 
+	 * @throws CloneNotSupportedException this should never happen in practice
+	 */
+	public CloneableIterator<E> clone() throws CloneNotSupportedException;
+
+}
