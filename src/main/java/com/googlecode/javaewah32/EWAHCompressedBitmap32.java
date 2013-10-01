f0223a20f7158abd96bf7d74c3a58f48158ae70e
Daniel Lemire <lemire@gmail.com>
2013-10-01 15:43:38 -0400

    Ooops

--- src/main/java/com/googlecode/javaewah32/EWAHCompressedBitmap32.java
+++ src/main/java/com/googlecode/javaewah32/EWAHCompressedBitmap32.java
@@ -19,12 +19,6 @@ import com.googlecode.javaewah.LogicalElement;
  * </p>
  * 
  * <p>
- * In contrast witht the 64-bit EWAH scheme (javaewah.EWAHCompressedBitmap), you
- * can expect this class to compress better, but to be slower at processing the
- * data. In effect, there is a trade-off between memory usage and performances.
- * </p>
- * 
- * <p>
  * In contrast with the 64-bit EWAH scheme (javaewah.EWAHCompressedBitmap), you
  * can expect this class to compress better, but to be slower at processing the
  * data. In effect, there is a trade-off between memory usage and performances.
