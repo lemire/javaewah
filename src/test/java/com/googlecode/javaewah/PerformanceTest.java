8d434383f6d5c12171bcfdcb1e19a767778c27d2
gssiyankai <syk.greg@gmail.com>
2014-12-30 23:14:20 +0100

    Persist benchmark results in h2 db file

--- src/test/java/com/googlecode/javaewah/PerformanceTest.java
+++ src/test/java/com/googlecode/javaewah/PerformanceTest.java
@@ -1,7 +1,11 @@
 package com.googlecode.javaewah;
 
+import java.io.File;
 import java.util.Arrays;
 
+import com.carrotsearch.junitbenchmarks.IResultsConsumer;
+import com.carrotsearch.junitbenchmarks.WriterConsumer;
+import com.carrotsearch.junitbenchmarks.h2.H2Consumer;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,53 +14,54 @@ import org.junit.rules.TestRule;
 import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
 import com.carrotsearch.junitbenchmarks.BenchmarkRule;
 
-public class PerformanceTest
-{
-	  @Rule
-	  public TestRule benchmarkRun = new BenchmarkRule();
-	 
-	  @BenchmarkOptions(benchmarkRounds = 3, warmupRounds = 3)
-	  @Test
-	  public void bigunion() throws Exception {
-	  	for(int k = 1; k < N ; k+=10) {
-				EWAHCompressedBitmap bitmapor = EWAHCompressedBitmap.or(Arrays.copyOf(ewah, k+1));
-				bogus += bitmapor.cardinality();
-	  	}
-	  }
-	  
+public class PerformanceTest {
 
-	  @BenchmarkOptions(benchmarkRounds = 3, warmupRounds = 3)
-	  @Test
-	  public void toarray() throws Exception {
-	  	for(int k = 1; k < N * 100 ; ++k) {
-	  		bogus += ewah[k % N].toArray().length;
-	  	}
-	  }
+    static final IResultsConsumer CONSOLE_CONSUMER = new WriterConsumer();
+    static final IResultsConsumer H2_CONSUMER = new H2Consumer(new File("PerformanceBenchmarks"));
 
-	  @BenchmarkOptions(benchmarkRounds = 3, warmupRounds = 3)
-	  @Test
-	  public void cardinality() throws Exception {
-	  	for(int k = 1; k < N * 100; ++k) {
-	  		bogus += ewah[k % N].cardinality();
-	  	}
-	  }
+    @Rule
+    public TestRule benchmarkRun = new BenchmarkRule(CONSOLE_CONSUMER, H2_CONSUMER);
 
-	  
-	  @BeforeClass
-    public static void prepare()
-    {
-			for (int k = 0; k < N; ++k) {
-				ewah[k] = new EWAHCompressedBitmap();
-				for (int x = 0; x < M; ++x) {
-					ewah[k].set(x*(N-k+2));
-				}
-				ewah[k].trim();
-			}
+    @BenchmarkOptions(benchmarkRounds = 3, warmupRounds = 3)
+    @Test
+    public void bigunion() throws Exception {
+        for (int k = 1; k < N; k += 10) {
+            EWAHCompressedBitmap bitmapor = EWAHCompressedBitmap.or(Arrays.copyOf(ewah, k + 1));
+            bogus += bitmapor.cardinality();
+        }
     }
-	  static int N = 1000;
-	  static int M = 1000;
 
-	  static EWAHCompressedBitmap[] ewah = new EWAHCompressedBitmap[N];
-		public static int bogus = 0;
+    @BenchmarkOptions(benchmarkRounds = 3, warmupRounds = 3)
+    @Test
+    public void toarray() throws Exception {
+        for (int k = 1; k < N * 100; ++k) {
+            bogus += ewah[k % N].toArray().length;
+        }
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 3, warmupRounds = 3)
+    @Test
+    public void cardinality() throws Exception {
+        for (int k = 1; k < N * 100; ++k) {
+            bogus += ewah[k % N].cardinality();
+        }
+    }
+
+
+    @BeforeClass
+    public static void prepare() {
+        for (int k = 0; k < N; ++k) {
+            ewah[k] = new EWAHCompressedBitmap();
+            for (int x = 0; x < M; ++x) {
+                ewah[k].set(x * (N - k + 2));
+            }
+            ewah[k].trim();
+        }
+    }
+
+    static int N = 1000;
+    static int M = 1000;
+
+    static EWAHCompressedBitmap[] ewah = new EWAHCompressedBitmap[N];
+    public static int bogus = 0;
 }
-
